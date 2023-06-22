e1b120ec85bff5aae7be99b0f8381260f1315c76
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-06-22 16:45:52 -0300

    Resolving MicroBlog exercise

---
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -0,0 +1,13 @@
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+class MicroBlog {
+    public String truncate(String input) {
+        if (input.codePointCount(0, input.length()) >= 5){
+             return input.substring(0,input.offsetByCodePoints(0,5));
+        }
+        return input;
+    }
+}
+
+
